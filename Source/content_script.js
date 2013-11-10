830d8eb20c44d8aeb9fe9d81152ae1923fdda7f6
Collin Donahue-Oponski <collindo@gmail.com>
2013-11-09 23:01:14 -0700

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,3 +1,22 @@
+;function() {
+
+var substitutions = [
+	[/\bwitnesses\b/g, "these dudes I know"],
+	[/\ballegedly\b/g, "kinda probably"],
+	[/\bnew study\b/g, "tumblr post"],
+	[/\brebuild\b/g, "avenge"],
+	[/\bspace\b/g, "spaaace"],
+	[/\bgoogle glass\b/g, "virtual boy"],
+	[/\bsmartphone\b/g, "pokédex"],
+	[/\belectric\b/g, "atomic"],
+	[/\bsenator\b/g, "elf-lord"],
+	[/\bcar\b/g, "cat"],
+	[/\belection\b/g, "eating contest"],
+	[/\bcongressional leaders\b/g, "river spirits"],
+	[/\bhomeland security\b/g, "homestar runner"],
+	[/\bcould not be reached for comment\b/g, "is guilty and everyone knows it"]
+];
+
 walk(document.body);
 
 function walk(node) 
@@ -31,12 +50,12 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	for (var i = 0; i < substitutions.length; i++) {
+		v.replace(substitions[i][0], substitions[i][1]);
+	}
 	
 	textNode.nodeValue = v;
 }
 
 
+}();
